15f8d77cc21f34bd1464ecb00820374b33c92d4e
as116 <annika.stampf@uni-ulm.de>
2023-09-14 12:12:48 +0200

    update data and add random paramater combination generator

--- js/site.js
+++ js/site.js
@@ -7,6 +7,8 @@ var filterActive = false;
 var filteredData;
 var filteredInteractionData;
 
+var highlightColorArticles = 'lightgrey';
+
 
 
 loadData();
@@ -14,7 +16,7 @@ loadData();
 
 // Function to load the JSON data from the file
 function loadData() {
-    fetch('../data/slrarticle.json')
+    fetch('./data/articlesslr.json')
         .then(response => response.json())
         .then(data => {
             jsonData = data.filter(entry => {
@@ -22,36 +24,67 @@ function loadData() {
             });
             filterAndDisplayData();
         });
-
-   
-
 }
+
 function createInteractionData(data) {
     const result = [];
+
+    // Define the keys you want to consider
+    const keysToConsider = [
+        "Interaction Partner A",
+        "Interaction Partner A - Other",
+        "Role of Interaction Partner A",
+        "Interaction Partner B",
+        "Interaction Partner B - Other",
+        "Role of Interaction Partner B",
+        "Situation - Traffic Density",
+        "Situation - Traffic Density B",
+        "Situation - Lane Number",
+        "Situation - Lane Number B",
+        "Situation - Traffic Autonomy",
+        "Situation - Traffic Autonomy B",
+        "Situation - Lane Setting",
+        "Situation - Significant Other Situation Conditions",
+        "Interaction Relationship - Entity Relationship",
+        "Interaction Relationship - Interaction Mapping",
+        "Interaction Relationship - Interaction Flow",
+        "Interaction Relationship - Position Dependency",
+        "Interaction Relationship - Time Synchronicity"
+    ];
 
     data.forEach(entry => {
         let newEntries = [entry];
 
         // Iterate through the keys in the entry
         for (const key in entry) {
-            // Check if the value is a string and contains ","
-            if (typeof entry[key] === 'string' && entry[key].includes(', ')) {
-                const values = entry[key].split(', ').map(value => value.trim());
+            // Check if the key is one of the keys to consider
+            if (keysToConsider.includes(key)) {
 
-                // Create new entries based on the split values
-                const tempEntries = [];
-                newEntries.forEach(newEntry => {
-                    values.forEach(value => {
-                        const clonedEntry = { ...newEntry };
-                        clonedEntry[key] = value;
-                        tempEntries.push(clonedEntry);
+                // Remove " (define)" from the value and trim spaces
+                let cleanedValue = entry[key].replace(" (define)", "").trim();
+
+                // Check if the value is a string and contains ","
+                if (typeof cleanedValue === 'string' && cleanedValue.includes(', ')) {
+
+                    const values = cleanedValue.split(', ').map(value => value.trim());
+
+                    // Create new entries based on the split values
+                    const tempEntries = [];
+                    newEntries.forEach(newEntry => {
+                        values.forEach(value => {
+                            const clonedEntry = { ...newEntry };
+                            clonedEntry[key] = value;
+                            tempEntries.push(clonedEntry);
+                        });
                     });
-                });
 
-                newEntries = tempEntries;
+                    newEntries = tempEntries;
+                } else {
+                    // If the value doesn't need to be split, still update it to the cleaned value
+                    entry[key] = cleanedValue;
+                }
             }
         }
-
         result.push(...newEntries);
     });
 
@@ -90,9 +123,10 @@ function filterAndDisplayData() {
             var interactionPartnerB = entry['Interaction Partner B'].toLowerCase();
             var roleA = entry['Role of Interaction Partner A'].toLowerCase();
             var roleB = entry['Role of Interaction Partner B'].toLowerCase();
-            var trafficDensityA = entry['Situation - Traffic Density A'].toLowerCase();
-            var laneNumberA = entry['Situation - Lane Number A'].toLowerCase();
-            var trafficAutonomy = entry['Situation - Traffic Autonomy A'].toLowerCase();
+            var trafficDensityA = entry['Situation - Traffic Density'].toLowerCase();
+            var laneNumberA = entry['Situation - Lane Number'].toLowerCase();
+            var laneSettingA = entry['Situation - Lane Setting'].toLowerCase();
+            var trafficAutonomy = entry['Situation - Traffic Autonomy'].toLowerCase();
             var entityRelationship = entry['Interaction Relationship - Entity Relationship'] ? entry['Interaction Relationship - Entity Relationship'].toLowerCase() : "not applicable";
             var interactionMapping = entry['Interaction Relationship - Interaction Mapping'].toLowerCase();
             var interactionFlow = entry['Interaction Relationship - Interaction Flow'].toLowerCase();
@@ -111,6 +145,7 @@ function filterAndDisplayData() {
                         roleB.includes(filter) ||
                         trafficDensityA.includes(filter) ||
                         laneNumberA.includes(filter) ||
+                        laneSettingA.includes(filter) ||
                         trafficAutonomy.includes(filter) ||
                         entityRelationship.includes(filter) ||
                         interactionMapping.includes(filter) ||
@@ -147,8 +182,21 @@ function displayData() {
         titleButton.className = 'list-group-item list-group-item-action';
         titleButton.setAttribute('data-bs-toggle', 'collapse');
         titleButton.setAttribute('data-bs-target', '#details-' + index);
-        titleButton.setAttribute('id', entry['Article ID']);
-        titleButton.textContent = entry['Article Title'];
+        titleButton.setAttribute('id', entry['Unique Concept ID']);
+        // Get the Article Title and Concept Number from the entry object
+        const articleTitle = entry['Article Title'];
+        const conceptNumber = entry['Concept Number'];
+
+        // Initialize combinedText with the Article Title
+        let combinedText = articleTitle;
+
+        // Conditionally append Concept Number if it's not an empty string
+        if (conceptNumber !== "") {
+            combinedText += ` (${conceptNumber})`;
+        }
+
+        // Set the combined text as the content of the titleButton
+        titleButton.textContent = combinedText;
 
         // Create a div for the details
         var detailsDiv = document.createElement('div');
@@ -158,24 +206,32 @@ function displayData() {
 
         // Add the other parameters (excluding the specified ones)
         for (var key in entry) {
-            if (key !== 'Article ID' && key !== 'Resolve?'  && key !== 'User Notes' && key !== 'Notes' && key !== 'Status' && key !== 'User Count' && key !== 'User Name' && key !== 'Include' && key !== 'Article Title') {
-                var para = document.createElement('p');
-                para.style.margin = '4px 0'; // Reduce spacing between elements
+            if (key !== 'Article ID' && key !== 'Resolve?' && key !== 'Concept Number' && key !== 'Unique Concept ID' && key !== 'BibTex Key' && key !== 'User Note' && key !== 'Notes' && key !== 'Status' && key !== 'User Count' && key !== 'User Name' && key !== 'Include' && key !== 'Article Title' && key !== 'Interaction Modalities') {
 
-                // Check if the value is a link
-                var value = entry[key];
-                if (typeof value === 'string' && (value.startsWith('http://') || value.startsWith('https://'))) {
-                    var link = document.createElement('a');
-                    link.href = value;
-                    link.textContent = value;
-                    link.target = '_blank'; // Open in a new tab
-                    para.appendChild(document.createTextNode(key + ': '));
-                    para.appendChild(link);
-                } else {
-                    para.textContent = key + ': ' + value;
+                // Check if entry[key] is not an empty string
+                if (entry[key] !== "") {
+                    var para = document.createElement('p');
+                    para.style.margin = '4px 0'; // Reduce spacing between elements
+
+                    // Create a strong element for bold text
+                    var strong = document.createElement('strong');
+                    strong.textContent = key + ': ';
+                    para.appendChild(strong);
+
+                    // Check if the value is a link
+                    var value = entry[key];
+                    if (typeof value === 'string' && (value.startsWith('http://') || value.startsWith('https://'))) {
+                        var link = document.createElement('a');
+                        link.href = value;
+                        link.textContent = value;
+                        link.target = '_blank'; // Open in a new tab
+                        para.appendChild(link);
+                    } else {
+                        para.appendChild(document.createTextNode(value));
+                    }
+
+                    detailsDiv.appendChild(para);
                 }
-
-                detailsDiv.appendChild(para);
             }
         }
 
@@ -187,7 +243,7 @@ function displayData() {
 
     // Update the entry count
     var entryCount = document.getElementById('entry-count');
-    entryCount.textContent = 'Papers found: ' + filteredData.length;
+    entryCount.textContent = 'Cross-Traffic Interaction Concepts found: ' + filteredData.length;
     createParallelCategories();
     createArcDiagram();
 }
@@ -197,10 +253,10 @@ function displayData() {
 function createParallelCategories() {
     var colors = Array(filteredInteractionData.length).fill('lightgray');
     // Extract data from filteredData
-    var lanes = filteredInteractionData.map(entry => entry['Situation - Lane Number A']);
-    var setting = filteredInteractionData.map(entry => entry['Situation - Lane Setting A']);
-    var density = filteredInteractionData.map(entry => entry['Situation - Traffic Density A']);
-    var autonomy = filteredInteractionData.map(entry => entry['Situation - Traffic Autonomy A']);
+    var lanes = filteredInteractionData.map(entry => entry['Situation - Lane Number']);
+    var setting = filteredInteractionData.map(entry => entry['Situation - Lane Setting']);
+    var density = filteredInteractionData.map(entry => entry['Situation - Traffic Density']);
+    var autonomy = filteredInteractionData.map(entry => entry['Situation - Traffic Autonomy']);
 
     var entityRel = filteredInteractionData.map(entry => entry['Interaction Relationship - Entity Relationship'] ? entry['Interaction Relationship - Entity Relationship'].toLowerCase() : "not applicable");
     var mapping = filteredInteractionData.map(entry => entry['Interaction Relationship - Interaction Mapping']);
@@ -250,15 +306,15 @@ function createParallelCategories() {
 
         for (var i = 0; i < points_data.points.length; i++) {
             var clickedData = filteredInteractionData[points_data.points[i].pointNumber];
-            console.log(clickedData['Article ID']);
-            var articleID = clickedData['Article ID'];
+            console.log(clickedData['Unique Concept ID']);
+            var articleID = clickedData['Unique Concept ID'];
 
             // Update colors for both containers but use different highlight colors
             new_color_S[points_data.points[i].pointNumber] = highlightColorS;
             new_color_IR[points_data.points[i].pointNumber] = highlightColorIR;
 
             // Update background color based on which container was clicked
-            document.getElementById(articleID).style.backgroundColor = containerID === 'S-container' ? highlightColorS : highlightColorIR;
+            document.getElementById(articleID).style.backgroundColor = highlightColorArticles;
 
             highlightArcLinks(clickedData);
         }
@@ -275,15 +331,6 @@ function createParallelCategories() {
     gd.on('plotly_click', function (points_data) { update_color(points_data, 'S-container'); });
     gdIR.on('plotly_click', function (points_data) { update_color(points_data, 'IR-container'); });
 
-
-
-
-
-
-
-
-    
-
 }
 
 
@@ -294,7 +341,7 @@ function createArcDiagram() {
     d3.select("#arcDiagram").selectAll("*").remove();
 
     // Set the dimensions and margins of the graph
-    var margin = { top: 0, right: 30, bottom: 200, left: 60 },
+    var margin = { top: 0, right: 30, bottom: 200, left: 90 },
         width = 800 - margin.left - margin.right,
         height = 600 - margin.top - margin.bottom;
 
@@ -328,12 +375,24 @@ function createArcDiagram() {
             nodes.push({ name: ipB });
         }
 
+        //// Check for unique links
+        //var linkKey = [ipA, ipB].sort().join('-');
+        //if (!linkSet.has(linkKey)) {
+        //    links.push({
+        //        source: ipA,
+        //        target: ipB
+        //    });
+        //    linkSet.add(linkKey);
+        //}
+
+
         // Check for unique links
         var linkKey = [ipA, ipB].sort().join('-');
         if (!linkSet.has(linkKey)) {
             links.push({
                 source: ipA,
-                target: ipB
+                target: ipB,
+                selfLink: ipA === ipB // Add this line to identify self-links
             });
             linkSet.add(linkKey);
         }
@@ -358,16 +417,24 @@ function createArcDiagram() {
         .data(links)
         .enter()
         .append('path')
-        .attr('class', 'mylinks') 
+        .attr('class', 'mylinks')
         .attr('d', function (d) {
-            start = x(d.source);    // X position of start node on the X axis
-            end = x(d.target);      // X position of end node
-            return ['M', start, height - 30,
-                'A',
-                (start - end) / 2, ',',
-                (start - end) / 2, 0, 0, ',',
-                start < end ? 1 : 0, end, ',', height - 30]
-                .join(' ');
+            if (d.selfLink) { // Handle self-links
+                const radius = size(nodeCounts[d.source]);
+                return `M ${x(d.source)} ${height - 30} 
+                    a ${radius} ${radius} 0 1 0 ${radius * 2} 0 
+                    a ${radius} ${radius} 0 1 0 ${-radius * 2} 0`;
+            } else {
+                // Existing logic for normal links
+                start = x(d.source);
+                end = x(d.target);
+                return ['M', start, height - 30,
+                    'A',
+                    (start - end) / 2, ',',
+                    (start - end) / 2, 0, 0, ',',
+                    start < end ? 1 : 0, end, ',', height - 30]
+                    .join(' ');
+            }
         })
         .style("fill", "none")
         .attr("stroke", "lightgrey")
@@ -438,8 +505,8 @@ function createArcDiagram() {
                     ipB === d.name.trim()) {
                     currentColorsS[index] = '#80B3AC';
                     currentColorsIR[index] = '#9EC1A3';
-                    var articleID = entry['Article ID'];
-                    document.getElementById(articleID).style.backgroundColor = "#007bff";
+                    var articleID = entry['Unique Concept ID'];
+                    document.getElementById(articleID).style.backgroundColor = highlightColorArticles;
                 }
             });
 
